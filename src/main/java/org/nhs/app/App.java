4bc3c9f39b466736e2776c6c8395cdef794b803e
yghadge <1118259@apps.nsd.org>
2024-10-07 21:17:13 +0000

    done

--- src/main/java/org/nhs/app/App.java
+++ src/main/java/org/nhs/app/App.java
@@ -1,8 +1,53 @@
-/*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for license information.
- *---------------------------------------------------------------------------------------*/
+public class App{
 
-package org.nhs.app;
+    public static void main(String args[]){
+        int lim = 1;
+        for (int number = 1; number <= 5; number++){
+            for (int counter = 1; counter <= lim; counter++){
+                System.out.print(number);
+                
+            }
+            System.out.println();
+            lim += 1;
+        }
+        System.out.println();
+        two();
+        System.out.println();
+        three();
+    }
 
-// Your code goes here
+    public static void two(){
+        for (int number = 1; number <= 5; number++){
+            for (int dot = 5 - number; dot > 0; dot--){
+                System.out.print(".");
+            }
+            
+            for (int counter = number; counter > 0; counter--){
+                System.out.print(number);
+            }
+
+            System.out.println();
+        }
+
+
+    }
+
+    public static void three(){
+        for (int number = 1; number <= 5; number++){
+            for (int dot = 5 - number; dot > 0; dot--){
+                System.out.print(".");
+            }
+
+            System.out.print(number);
+
+            for (int dot = number - 1; dot > 0; dot--){
+                System.out.print(".");
+            }
+
+            System.out.println();
+        }
+
+    }
+
+
+}
